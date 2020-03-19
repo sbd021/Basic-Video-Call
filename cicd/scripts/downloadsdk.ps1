568ed349dcd97a5121f2e36429455fcff4e1e4c8
sbd021 <sbd021@126.com>
2020-03-19 12:17:57 +0800

    ps

--- cicd/scripts/downloadsdk.ps1
+++ cicd/scripts/downloadsdk.ps1
@@ -1,4 +1,2 @@
 param($SDKURL)
 Invoke-WebRequest -Uri "$SDKURL" -OutFile .\AgoraSDK.zip -TimeoutSec 10;
-
-Invoke-WebRequest -Uri "http://download.qt.io/archive/qt/5.12/5.12.7/qt-opensource-windows-x86-5.12.7.exe" -OutFile .\qt-5.12.7.exe -TimeoutSec 10;
