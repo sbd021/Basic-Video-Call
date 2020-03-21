a5b6dfcdb647e2834e48c0c62399d0d54c1b399c
sbd021 <sbd021@126.com>
2020-03-21 14:04:12 +0800

    x64

--- Group-Video/OpenVideoCall-Windows/build_azure_pipeline.bat
+++ Group-Video/OpenVideoCall-Windows/build_azure_pipeline.bat
@@ -2,8 +2,13 @@ cd %~1
 echo "build azure pipeline bat"
 echo %VS2017_BUILD%
 
-set QTDIR=C:\Qt\Qt5.12.7\5.12.7\msvc2017
-set QTDIR_64=C:\Qt\Qt5.12.7\5.12.7\msvc2017_64
+set Machine=%~2
+
+if Machine == x86 (
+  set QTDIR=C:\Qt\Qt5.12.7\5.12.7\msvc2017
+) else (
+  set QTDIR=C:\Qt\Qt5.12.7\5.12.7\msvc2017_64
+)
 echo %QTDIR%
 echo "==================================="
 call build_release.bat "%VS2017_BUILD%" "%QTDIR%" %~2
